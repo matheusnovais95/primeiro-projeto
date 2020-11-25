06269822a88f63d348a993549f596b87e65826bb
Matheus Campos <mths_campos@hotmail.com>
2020-11-25 10:31:35 -0300

    Entendo classes

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import OlaMundo from './components/OlaMundo';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <OlaMundo styleOne={styles.paragrafo} styleTwo={styles.msg} nome='Jonas'/>
     </View>
   );
 }
@@ -18,4 +19,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paragrafo: {
+    backgroundColor: '#993399',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+    padding: 10,
+    borderRadius: 5
+  },
+  msg: {
+    backgroundColor: '#C8A2C8',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 12,
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    textAlign: 'center'
+  }
 });
+
